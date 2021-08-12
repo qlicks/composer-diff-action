ece80fc3ec323c4fe6a08c2847a12c899574b27e
Misha Serbenyuk <80031477+MishaSerbenyuk@users.noreply.github.com>
2021-08-12 11:58:25 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,9 +5,9 @@ set -e
 OUTPUT=$(composer diff $*)
 echo "$OUTPUT"
 
-#OUTPUT=$(echo "$OUTPUT" | sed 's/\x1B\[[0-9;]\{1,\}[A-Za-z]//g')
+OUTPUT=$(echo "$OUTPUT" | sed 's/\x1B\[[0-9;]\{1,\}[A-Za-z]//g')
 #OUTPUT="${OUTPUT//'%'/'%25'}"
-#OUTPUT="${OUTPUT//$'\n'/'%0A'}"
-#OUTPUT="${OUTPUT//$'\r'/'%0D'}"
+OUTPUT="${OUTPUT//$'\n'/'%0A'}"
+OUTPUT="${OUTPUT//$'\r'/'%0D'}"
 
 echo "::set-output name=composer_diff::$OUTPUT"
