2ef4c4db48807580d28716655fff0f1eb09c2259
Misha Serbenyuk <80031477+MishaSerbenyuk@users.noreply.github.com>
2021-08-17 16:33:04 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ echo "$OUTPUT"
 
 OUTPUT=$(echo "$OUTPUT" | sed 's/\x1B\[[0-9;]\{1,\}[A-Za-z]//g')
 #OUTPUT="${OUTPUT//'%'/'%25'}"
-OUTPUT="${OUTPUT//$'\n'/'%0A'}"
+#OUTPUT="${OUTPUT//$'\n'/'%0A'}"
 #OUTPUT="${OUTPUT//$'\r'/'%0D'}"
 
 
