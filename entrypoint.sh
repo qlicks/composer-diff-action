845f20e5ffad9c462f7d585d1160d651d5519071
Misha Serbenyuk <80031477+MishaSerbenyuk@users.noreply.github.com>
2021-08-17 15:02:01 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,9 +6,9 @@ OUTPUT=$(composer diff $*)
 echo "$OUTPUT"
 
 OUTPUT=$(echo "$OUTPUT" | sed 's/\x1B\[[0-9;]\{1,\}[A-Za-z]//g')
-OUTPUT="${OUTPUT//'%'/'%25'}"
-OUTPUT="${OUTPUT//$'\n'/'%0A'}"
-OUTPUT="${OUTPUT//$'\r'/'%0D'}"
+#OUTPUT="${OUTPUT//'%'/'%25'}"
+#OUTPUT="${OUTPUT//$'\n'/'%0A'}"
+#OUTPUT="${OUTPUT//$'\r'/'%0D'}"
 
 PR_NUMBER=$(echo $CI_REF | awk 'BEGIN { FS = "/" } ; { print $3 }')
 
