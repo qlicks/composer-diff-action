bd390174eebdee8e1f276261f2f680c752a75fb0
Misha Serbenyuk <80031477+MishaSerbenyuk@users.noreply.github.com>
2021-08-17 15:22:34 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,7 +10,7 @@ OUTPUT=$(echo "$OUTPUT" | sed 's/\x1B\[[0-9;]\{1,\}[A-Za-z]//g')
 #OUTPUT="${OUTPUT//$'\n'/'%0A'}"
 #OUTPUT="${OUTPUT//$'\r'/'%0D'}"
 
-PR_NUMBER=$(echo $CI_REF | awk 'BEGIN { FS = "/" } ; { print $3 }')
+#PR_NUMBER=$(echo $CI_REF | awk 'BEGIN { FS = "/" } ; { print $3 }')
 
 #curl -s -H "Authorization: token ${GITHUB_TOKEN} " -X POST -d '{"body": "${OUTPUT}"}' "${GITHUB_API_URL}/repos/${GITHUB_REPOSITORY_OWNER}/${CI_REPOSITORY_NAME}/issues/${PR_NUMBER}/comments"
 
