593e9f5351e0096c7f80869c362359ed0c1b4a76
Misha Serbenyuk <80031477+MishaSerbenyuk@users.noreply.github.com>
2021-08-17 16:21:43 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,9 +10,14 @@ OUTPUT=$(echo "$OUTPUT" | sed 's/\x1B\[[0-9;]\{1,\}[A-Za-z]//g')
 OUTPUT="${OUTPUT//$'\n'/'%0A'}"
 #OUTPUT="${OUTPUT//$'\r'/'%0D'}"
 
+
+echo '{"body": "'"$OUTPUT"'"}' > /tmp/composer.diff\
+
+cat /tmp/composer.diff
+
 PR_NUMBER=$(echo $CI_REF | awk 'BEGIN { FS = "/" } ; { print $3 }')
 
-curl -s -H "Authorization: token ${GITHUB_TOKEN} " -X POST -d @- "${GITHUB_API_URL}/repos/${GITHUB_REPOSITORY_OWNER}/${CI_REPOSITORY_NAME}/issues/${PR_NUMBER}/comments" << echo '{"body": "'"$OUTPUT"'"}'
+curl -s -H "Authorization: token ${GITHUB_TOKEN} " -X POST -d @/tmp/composer.diff "${GITHUB_API_URL}/repos/${GITHUB_REPOSITORY_OWNER}/${CI_REPOSITORY_NAME}/issues/${PR_NUMBER}/comments"
 
 echo "::set-output name=composer_diff::$OUTPUT"
-echo $OUTPUT > composer.diff
+
