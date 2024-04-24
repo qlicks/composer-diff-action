a5235b2c9d6827998b23426001ff9125ec1f41c5
dim-ivanchenko <121875682+dim-ivanchenko@users.noreply.github.com>
2024-04-24 18:38:57 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,23 +1,20 @@
 #!/bin/bash
 
+ARGS=$*
+OUTPUT=$(composer diff --strict $ARGS)
+EXIT_CODE=$?
+
 set -e
 
-OUTPUT=$(composer diff $*)
 echo "$OUTPUT"
 
 echo "composer_diff_exit_code=$EXIT_CODE" >> $GITHUB_OUTPUT
 
-OUTPUT=$(echo "$OUTPUT" | sed 's/\x1B\[[0-9;]\{1,\}[A-Za-z]//g')
-#OUTPUT="${OUTPUT//'%'/'%25'}"
-#OUTPUT="${OUTPUT//$'\n'/'\n'}"
-#OUTPUT="${OUTPUT//$'\r'/'%0D'}"
+delimiter="$(openssl rand -hex 8)"
+echo "composer_diff<<${delimiter}" >> "${GITHUB_OUTPUT}"
+echo "${OUTPUT}" >> "${GITHUB_OUTPUT}"
+echo "${delimiter}" >> "${GITHUB_OUTPUT}"
 
-
-echo "$OUTPUT" > $GITHUB_WORKSPACE/composer.diff
-
-#PR_NUMBER=$(echo $CI_REF | awk 'BEGIN { FS = "/" } ; { print $3 }')
-
-#curl -s -H "Authorization: token ${GITHUB_TOKEN} " -X POST -d @/tmp/composer.diff "${GITHUB_API_URL}/repos/${GITHUB_REPOSITORY_OWNER}/${CI_REPOSITORY_NAME}/issues/${PR_NUMBER}/comments"
-
-echo "::set-output name=composer_diff::$OUTPUT"
-
+if [[ "$*" == *"--strict"* ]]; then
+  exit $EXIT_CODE
+fi
